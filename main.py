722394d0ddf1dad8f4743a17f561086e808562b9
404moneycoin-glitch <404moneycoin@gmail.com>
2026-09-12 00:02:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ TRADE_SIZE = 25.00
 CHECK_EVERY = 300
 app = Flask(__name__)
 latest_signals = {}
+@app.route("/signals")
+def signals():
+    return jsonify(latest_signals)
 cash = STARTING_CASH
 positions = {}
 wins = 0
