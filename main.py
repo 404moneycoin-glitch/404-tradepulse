6895a67eb3fd0936aaed0b806876c353b926966b
404moneycoin-glitch <404moneycoin@gmail.com>
2026-09-13 13:21:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ STARTING_CASH = 100.00
 TRADE_SIZE = 25.00
 CHECK_EVERY = 300
 app = Flask(__name__)
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    return response
 latest_signals = {}
 @app.route("/signals")
 def signals():
